50dae6e55fce8686eb5b0dc4c6a50ea570e156af
Denys Zamorniak <dzamorniak@gmail.com>
2025-10-07 21:09:10 +0200

    Initial Commit

---
+++ SystemEQ for Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SystemEQ for Mac
+//
+//  Created by Denys Zamorniak on 07/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SystemEQ for Mac/SystemEQ_for_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemEQ_for_MacApp.swift
+//  SystemEQ for Mac
+//
+//  Created by Denys Zamorniak on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SystemEQ_for_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
